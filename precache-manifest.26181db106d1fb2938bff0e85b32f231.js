61cdc1d4b9f3e9d1c865717b1fd5a4a3ac0a4637
ericzlmd <ericzlmd@gmail.com>
2020-02-04 20:43:38 -0500

    Updates

---
+++ precache-manifest.26181db106d1fb2938bff0e85b32f231.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ffe7d0a1f3d5320bdfc14bb3fc9cae3",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "e89ba1f79aeb1365c6ad",
+    "url": "/monsters-rolodex/static/css/main.63d3575b.chunk.css"
+  },
+  {
+    "revision": "5972ab9a24d87de2b217",
+    "url": "/monsters-rolodex/static/js/2.40aee04e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.40aee04e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e89ba1f79aeb1365c6ad",
+    "url": "/monsters-rolodex/static/js/main.30fc922b.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
